17714a6502a97f21f52196de3c90dbeceb46b4ce
Daniel-Cross <crossy1686@gmail.com>
2018-10-04 01:20:56 +0100

    added tab functionality

--- app/src/components/MilitaryPress.js
+++ app/src/components/MilitaryPress.js
@@ -20,52 +20,240 @@ class MilitaryPress extends Component {
   };
 
   render() {
-    const weeks = [
+    const workouts = [
       {
-        week: 1,
-        Rep: 5
+        name: 'Session 1',
+        reps: [5, 5, 3, 5, 5, 5]
+      },
+      {
+        name: 'Session 2',
+        reps: [5, 5, 3, 3, 3, 3]
+      },
+      {
+        name: 'Session 3',
+        reps: [5, 5, 3, 5, 3, 1]
+      },
+      {
+        name: 'De-Load',
+        reps: [5, 5, 5, 5, 5, 5]
       }
     ];
 
-    return (
-      <div className="MilitaryPress">
-        <div className="home-button">
-          <Link to="/" className="back-link">
-            <Button
-              variant="extendedFab"
-              aria-label="Delete"
-              className="back-button"
-              color="secondary"
+    if (this.state.value === 0) {
+      return (
+        <div className="MilitaryPress">
+          <div className="home-button">
+            <Link to="/" className="back-link">
+              <Button
+                variant="extendedFab"
+                aria-label="Delete"
+                className="back-button"
+                color="secondary"
+              >
+                Home
+              </Button>
+            </Link>
+          </div>
+          <div className="tag">
+            <h2>Military Press</h2>
+          </div>
+          <Paper className="workout-cycle-tabs">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              centered
             >
-              Home
-            </Button>
-          </Link>
-        </div>
-        <div className="tag">
-          <h2>Military Press</h2>
-        </div>
-        <Paper className="workout-cycle-tabs">
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="secondary"
-            textColor="secondary"
-            centered
-          >
-            <Tab label="Cycle One" />
-            <Tab label="Cycle Two" />
-            <Tab label="Cycle Three" />
-            <Tab label="Cycle Four" />
-            <Tab label="Cycle Five" />
-          </Tabs>
-        </Paper>
-        <div className="cycle-container">
-          <Paper className="cycle">
-            Reps: 5 Reps: 5 Reps: 3 Reps: 5 Reps: 5 Reps: 5
+              <Tab label="Cycle One" />
+              <Tab label="Cycle Two" />
+              <Tab label="Cycle Three" />
+              <Tab label="Cycle Four" />
+              <Tab label="Cycle Five" />
+            </Tabs>
           </Paper>
+
+          <div className="cycle-container">
+            <Paper className="cycle">
+              <h2 className="session-title">Session 1</h2>
+              Reps: 5<br /> Reps: 5<br /> Reps: 3<br /> Reps: 5<br /> Reps: 5
+              <br /> Reps: 5
+            </Paper>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else if (this.state.value === 1) {
+      return (
+        <div className="MilitaryPress">
+          <div className="home-button">
+            <Link to="/" className="back-link">
+              <Button
+                variant="extendedFab"
+                aria-label="Delete"
+                className="back-button"
+                color="secondary"
+              >
+                Home
+              </Button>
+            </Link>
+          </div>
+          <div className="tag">
+            <h2>Military Press</h2>
+          </div>
+          <Paper className="workout-cycle-tabs">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              centered
+            >
+              <Tab label="Cycle One" />
+              <Tab label="Cycle Two" />
+              <Tab label="Cycle Three" />
+              <Tab label="Cycle Four" />
+              <Tab label="Cycle Five" />
+            </Tabs>
+          </Paper>
+
+          <div className="cycle-container">
+            <Paper className="cycle">
+              Reps: 5<br /> Reps: 5<br /> Reps: 3<br /> Reps: 3<br /> Reps: 3
+              <br /> Reps: 3
+            </Paper>
+          </div>
+        </div>
+      );
+    } else if (this.state.value === 2) {
+      return (
+        <div className="MilitaryPress">
+          <div className="home-button">
+            <Link to="/" className="back-link">
+              <Button
+                variant="extendedFab"
+                aria-label="Delete"
+                className="back-button"
+                color="secondary"
+              >
+                Home
+              </Button>
+            </Link>
+          </div>
+          <div className="tag">
+            <h2>Military Press</h2>
+          </div>
+          <Paper className="workout-cycle-tabs">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              centered
+            >
+              <Tab label="Cycle One" />
+              <Tab label="Cycle Two" />
+              <Tab label="Cycle Three" />
+              <Tab label="Cycle Four" />
+              <Tab label="Cycle Five" />
+            </Tabs>
+          </Paper>
+
+          <div className="cycle-container">
+            <Paper className="cycle">
+              Reps: 5<br /> Reps: 5<br /> Reps: 3<br /> Reps: 5<br /> Reps: 3
+              <br /> Reps: 1
+            </Paper>
+          </div>
+        </div>
+      );
+    } else if (this.state.value === 3) {
+      return (
+        <div className="MilitaryPress">
+          <div className="home-button">
+            <Link to="/" className="back-link">
+              <Button
+                variant="extendedFab"
+                aria-label="Delete"
+                className="back-button"
+                color="secondary"
+              >
+                Home
+              </Button>
+            </Link>
+          </div>
+          <div className="tag">
+            <h2>Military Press</h2>
+          </div>
+          <Paper className="workout-cycle-tabs">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              centered
+            >
+              <Tab label="Cycle One" />
+              <Tab label="Cycle Two" />
+              <Tab label="Cycle Three" />
+              <Tab label="Cycle Four" />
+              <Tab label="Cycle Five" />
+            </Tabs>
+          </Paper>
+
+          <div className="cycle-container">
+            <Paper className="cycle">
+              Reps: 5<br /> Reps: 5<br /> Reps: 5<br /> Reps: 5<br /> Reps: 5
+              <br /> Reps: 5
+            </Paper>
+          </div>
+        </div>
+      );
+    } else if (this.state.value === 4) {
+      return (
+        <div className="MilitaryPress">
+          <div className="home-button">
+            <Link to="/" className="back-link">
+              <Button
+                variant="extendedFab"
+                aria-label="Delete"
+                className="back-button"
+                color="secondary"
+              >
+                Home
+              </Button>
+            </Link>
+          </div>
+          <div className="tag">
+            <h2>Military Press</h2>
+          </div>
+          <Paper className="workout-cycle-tabs">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              centered
+            >
+              <Tab label="Cycle One" />
+              <Tab label="Cycle Two" />
+              <Tab label="Cycle Three" />
+              <Tab label="Cycle Four" />
+              <Tab label="Cycle Five" />
+            </Tabs>
+          </Paper>
+
+          <div className="cycle-container">
+            <Paper className="cycle">
+              Gym
+              <br /> Reps: 5<br /> Reps: 3<br /> Reps: 5<br /> Reps: 5<br />{' '}
+              Reps: 5
+            </Paper>
+          </div>
+        </div>
+      );
+    } else {
+      null;
+    }
   }
 }
 
